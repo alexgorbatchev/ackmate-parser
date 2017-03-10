c5c2b32a7d214d32119824a64f931a7059fe933b
decaffeinate <alex.gorbatchev@gmail.com>
2017-03-10 14:47:49 -0800

    decaffeinate: Convert gulpfile.coffee and 1 other file to JS

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,24 @@
-# Load all required libraries.
-gulp = require 'gulp'
-gutil = require 'gulp-util'
-coffee = require 'gulp-coffee'
-istanbul = require 'gulp-istanbul'
-mocha = require 'gulp-mocha'
+// Load all required libraries.
+import gulp from 'gulp';
+import gutil from 'gulp-util';
+import coffee from 'gulp-coffee';
+import istanbul from 'gulp-istanbul';
+import mocha from 'gulp-mocha';
 
-gulp.task 'coffee', ->
-  gulp.src './src/**/*.coffee'
-    .pipe coffee({bare: true}).on('error', gutil.log)
-    .pipe gulp.dest './lib/'
+gulp.task('coffee', () =>
+  gulp.src('./src/**/*.coffee')
+    .pipe(coffee({bare: true}).on('error', gutil.log))
+    .pipe(gulp.dest('./lib/'))
+);
 
-gulp.task 'test', ['coffee'], ->
-  gulp.src ['lib/**/*.js']
-    .pipe(istanbul()) # Covering files
-    .on 'finish', ->
+gulp.task('test', ['coffee'], () =>
+  gulp.src(['lib/**/*.js'])
+    .pipe(istanbul()) // Covering files
+    .on('finish', () =>
       gulp.src(['test/**/*.spec.coffee'])
-        .pipe mocha reporter: 'spec', compilers: 'coffee:coffee-script', timeout: 5000
-        .pipe istanbul.writeReports() # Creating the reports after tests run
+        .pipe(mocha({reporter: 'spec', compilers: 'coffee:coffee-script', timeout: 5000}))
+        .pipe(istanbul.writeReports())
+  )
+); // Creating the reports after tests run
 
-gulp.task 'default', ['coffee']
+gulp.task('default', ['coffee']);
--- test/ackmate-parser.spec.js
+++ test/ackmate-parser.spec.js
@@ -1,48 +1,61 @@
-fs = require 'fs'
-Stream = require 'stream'
-{expect} = require 'chai'
-ackmateParser = require '../lib/ackmate-parser'
+import fs from 'fs';
+import Stream from 'stream';
+import { expect } from 'chai';
+import ackmateParser from '../lib/ackmate-parser';
 
-getFixtureStream = ->
-  fixture = fs.readFileSync "#{__dirname}/fixture.txt", 'utf8'
-  fixture = fixture.split ''
+let getFixtureStream = function() {
+  let fixture = fs.readFileSync(`${__dirname}/fixture.txt`, 'utf8');
+  fixture = fixture.split('');
 
-  stream = new Stream()
+  let stream = new Stream();
 
-  setTimeout ->
-    do push = ->
-      slice = fixture.splice 0, Math.round(Math.random() * 25) + 5
+  setTimeout(function() {
+    let push;
+    return push = function() {
+      let slice = fixture.splice(0, Math.round(Math.random() * 25) + 5);
 
-      if slice.length
-        stream.emit 'data', slice.join ''
-        setTimeout push, 2 if slice?
-      else
-        stream.emit 'end'
+      if (slice.length) {
+        stream.emit('data', slice.join(''));
+        if (slice != null) { return setTimeout(push, 2); }
+      } else {
+        return stream.emit('end');
+      }
+    }();
+  });
 
-  stream
+  return stream;
+};
 
-describe 'ackmate-parser', ->
-  describe '::fromStream', ->
-    it 'streams data', (done) ->
-      getFixtureStream().pipe stream = ackmateParser()
-      lines = []
+describe('ackmate-parser', () =>
+  describe('::fromStream', function() {
+    it('streams data', function(done) {
+      let stream;
+      getFixtureStream().pipe(stream = ackmateParser());
+      let lines = [];
 
-      stream.on 'data', ({filename, lineNumber, index, length, value}) ->
-        return lines.push "#{lineNumber}/#{index}/#{length}/#{value}" if length?
-        return lines.push "#{lineNumber}/#{value}" if value?
-        lines.push ">#{filename}"
+      stream.on('data', function({filename, lineNumber, index, length, value}) {
+        if (length != null) { return lines.push(`${lineNumber}/${index}/${length}/${value}`); }
+        if (value != null) { return lines.push(`${lineNumber}/${value}`); }
+        return lines.push(`>${filename}`);
+      });
 
-      stream.on 'end', ->
-        actual = lines.join '\n'
-        expect(actual).to.eql fs.readFileSync "#{__dirname}/expected.txt", 'utf8'
-        done()
+      return stream.on('end', function() {
+        let actual = lines.join('\n');
+        expect(actual).to.eql(fs.readFileSync(`${__dirname}/expected.txt`, 'utf8'));
+        return done();
+      });
+    });
 
-    it 'emits error', (done) ->
-      inputStream = getFixtureStream().pipe stream = ackmateParser()
+    return it('emits error', function(done) {
+      let stream;
+      let inputStream = getFixtureStream().pipe(stream = ackmateParser());
 
-      setTimeout ->
-        inputStream.emit 'error', new Error 'foo'
+      setTimeout(() => inputStream.emit('error', new Error('foo')));
 
-      stream.on 'error', (e) ->
-        expect(e).to.be.instanceOf Error
-        done()
+      return stream.on('error', function(e) {
+        expect(e).to.be.instanceOf(Error);
+        return done();
+      });
+    });
+  })
+);
