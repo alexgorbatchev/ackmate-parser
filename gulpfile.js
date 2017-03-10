9252b9092216d32bfcd0829ab66aaa2d6fe80ae1
Alex Gorbatchev <alex.gorbatchev@gmail.com>
2017-03-10 14:51:31 -0800

    Cleans up gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,27 @@
-// TODO: This file was created by bulk-decaffeinate.
-// Sanity-check the conversion and remove this comment.
-// Load all required libraries.
 import gulp from 'gulp';
 import gutil from 'gulp-util';
-import coffee from 'gulp-coffee';
+import sourcemaps from 'gulp-sourcemaps';
+import babel from 'gulp-babel';
 import istanbul from 'gulp-istanbul';
 import mocha from 'gulp-mocha';
 
-gulp.task('coffee', () =>
-  gulp.src('./src/**/*.coffee')
-    .pipe(coffee({bare: true}).on('error', gutil.log))
-    .pipe(gulp.dest('./lib/'))
+gulp.task('babel', () =>
+  gulp.src('./src/**/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(babel())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('lib'))
 );
 
-gulp.task('test', ['coffee'], () =>
+gulp.task('test', ['babel'], () =>
   gulp.src(['lib/**/*.js'])
     .pipe(istanbul()) // Covering files
     .on('finish', () =>
       gulp.src(['test/**/*.spec.coffee'])
         .pipe(mocha({reporter: 'spec', compilers: 'coffee:coffee-script', timeout: 5000}))
+        // Creating the reports after tests run
         .pipe(istanbul.writeReports())
   )
-); // Creating the reports after tests run
+);
 
 gulp.task('default', ['coffee']);
