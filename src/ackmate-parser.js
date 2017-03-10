047aecbaf3944f778bb7f77a29625c8e5f2c4833
Alex Gorbatchev <alex.gorbatchev@gmail.com>
2017-03-10 14:46:21 -0800

    Cleans up ackmate-parser.js

--- src/ackmate-parser.js
+++ src/ackmate-parser.js
@@ -1,49 +1,57 @@
-// TODO: This file was created by bulk-decaffeinate.
-// Sanity-check the conversion and remove this comment.
 import through2 from 'through2';
 
-export default function() {
-    let length, stream;
-    let filename     = null;
-    let tail         = null;
-    let surroundLine = /^(\d+):(.*)$/;
-    let matchLine    = /^(\d+);(\d+) (\d+):(.*)$/;
+export default function () {
+  let length, stream;
+  let filename = null;
+  let tail = null;
+  const surroundLine = /^(\d+):(.*)$/;
+  const matchLine = /^(\d+);(\d+) (\d+):(.*)$/;
+  const push = data => stream.push(data);
 
-    let push = data => stream.push(data);
+  function processLine(line) {
+    let lineNumber, matches, value;
 
-    let processLine = line => {
-      let lineNumber, matches, value;
-      if (line[0] === ':') {
-        filename = line.slice(1);
-        return push({filename});
+    if (line[0] === ':') {
+      filename = line.slice(1);
+      return push({ filename });
+    } else if (matches = line.match(surroundLine)) {
+      lineNumber = matches[matches.length - 2];
+      value = matches[matches.length - 1];
+      return push({ filename, lineNumber, value });
+    } else if (matches = line.match(matchLine)) {
+      const index = matches[matches.length - 3];
+      lineNumber = matches[matches.length - 4];
+      length = matches[matches.length - 2];
+      value = matches[matches.length - 1];
+      return push({ filename, lineNumber, index, length, value });
+    }
+  }
 
-      } else if (matches = line.match(surroundLine)) {
-        lineNumber = matches[matches.length - 2], value = matches[matches.length - 1];
-        return push({filename, lineNumber, value});
+  function transform(data, encoding, callback) {
+    data = data.toString();
 
-      } else if (matches = line.match(matchLine)) {
-        let index;
-        lineNumber = matches[matches.length - 4], index = matches[matches.length - 3], length = matches[matches.length - 2], value = matches[matches.length - 1];
-        return push({filename, lineNumber, index, length, value});
-      }
-    };
+    if (tail != null) {
+      data = tail + data;
+    }
 
-    let transform = function(data, encoding, callback) {
-      data    = data.toString();
-      if (tail != null) { data    = tail + data; }
-      let hasTail = data[data.length - 1] !== '\n';
-      let lines   = data.split('\n');
-      tail    = (hasTail && lines.pop()) || null;
+    let hasTail = data[data.length - 1] !== '\n';
+    let lines = data.split('\n');
 
-      for (let line of Array.from(lines)) { processLine(line); }
+    tail = (hasTail && lines.pop()) || null;
 
-      return callback();
-    };
+    for (let line of Array.from(lines)) {
+      processLine(line);
+    }
 
-    let flush = function(callback) {
-      if (tail != null) { processLine(tail); }
-      return callback();
-    };
+    return callback();
+  }
 
-    return stream = through2.obj(transform, flush);
-  };
+  function flush(callback) {
+    if (tail != null) {
+      processLine(tail);
+    }
+    return callback();
+  }
+
+  return stream = through2.obj(transform, flush);
+};
