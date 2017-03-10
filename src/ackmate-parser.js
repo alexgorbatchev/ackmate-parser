45fe43486631f57abdfe37809d9134bb33b56684
decaffeinate <alex.gorbatchev@gmail.com>
2017-03-10 14:39:54 -0800

    decaffeinate: Convert ackmate-parser.coffee to JS

--- src/ackmate-parser.js
+++ src/ackmate-parser.js
@@ -1,40 +1,47 @@
-through2 = require 'through2'
+import through2 from 'through2';
 
-module.exports = ->
-    filename     = null
-    tail         = null
-    surroundLine = /^(\d+):(.*)$/
-    matchLine    = /^(\d+);(\d+) (\d+):(.*)$/
+export default function() {
+    let length, stream;
+    let filename     = null;
+    let tail         = null;
+    let surroundLine = /^(\d+):(.*)$/;
+    let matchLine    = /^(\d+);(\d+) (\d+):(.*)$/;
 
-    push = (data) ->
-      stream.push data
+    let push = data => stream.push(data);
 
-    processLine = (line) =>
-      if line[0] is ':'
-        filename = line[1..]
-        push {filename}
+    let processLine = line => {
+      let lineNumber, matches, value;
+      if (line[0] === ':') {
+        filename = line.slice(1);
+        return push({filename});
 
-      else if matches = line.match surroundLine
-        [..., lineNumber, value] = matches
-        push {filename, lineNumber, value}
+      } else if (matches = line.match(surroundLine)) {
+        lineNumber = matches[matches.length - 2], value = matches[matches.length - 1];
+        return push({filename, lineNumber, value});
 
-      else if matches = line.match matchLine
-        [..., lineNumber, index, length, value] = matches
-        push {filename, lineNumber, index, length, value}
+      } else if (matches = line.match(matchLine)) {
+        let index;
+        lineNumber = matches[matches.length - 4], index = matches[matches.length - 3], length = matches[matches.length - 2], value = matches[matches.length - 1];
+        return push({filename, lineNumber, index, length, value});
+      }
+    };
 
-    transform = (data, encoding, callback) ->
-      data    = data.toString()
-      data    = tail + data if tail?
-      hasTail = data[data.length - 1] isnt '\n'
-      lines   = data.split '\n'
-      tail    = hasTail and lines.pop() or null
+    let transform = function(data, encoding, callback) {
+      data    = data.toString();
+      if (tail != null) { data    = tail + data; }
+      let hasTail = data[data.length - 1] !== '\n';
+      let lines   = data.split('\n');
+      tail    = (hasTail && lines.pop()) || null;
 
-      processLine line for line in lines
+      for (let line of Array.from(lines)) { processLine(line); }
 
-      callback()
+      return callback();
+    };
 
-    flush = (callback) ->
-      processLine tail if tail?
-      callback()
+    let flush = function(callback) {
+      if (tail != null) { processLine(tail); }
+      return callback();
+    };
 
-    stream = through2.obj transform, flush
+    return stream = through2.obj(transform, flush);
+  };
